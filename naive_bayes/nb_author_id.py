3bc66dd8bebade791eb47156018bf8fa2566c05b
aman-am <amanmehta015@gmail.com>
2020-01-17 00:10:07 +0530

    Naive Bayes

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,6 +14,7 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
 
 
 ### features_train and features_test are the features for the training
@@ -26,7 +27,18 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+gnb = GaussianNB()
 
+t0 = time()
+y_pred = gnb.fit(features_train, labels_train)
+print ("training time:", round(time()-t0, 3), "s")
+
+t0 = time()
+y_pred = gnb.predict(features_test)
+print ("predicting time:", round(time()-t0, 3), "s")
+
+accuracy = gnb.score(features_test, labels_test)
+print(accuracy)
 
 #########################################################
 
