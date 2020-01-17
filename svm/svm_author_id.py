28c2053089d61268300564ff55099c384506955d
aman-am <amanmehta015@gmail.com>
2020-01-17 09:58:03 +0530

    SVM

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -13,7 +13,7 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
-
+from sklearn.svm import SVC
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
@@ -24,6 +24,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+svc = SVC(kernel='linear')
+
+t0 = time()
+y_pred = svc.fit(features_train, labels_train)
+print ("training time:", round(time()-t0, 3), "s")
+
+t0 = time()
+y_pred = svc.predict(features_test)
+print ("predicting time:", round(time()-t0, 3), "s")
+
+accuracy = svc.score(features_test, labels_test)
+print(accuracy)
+
+
 
 #########################################################
 
