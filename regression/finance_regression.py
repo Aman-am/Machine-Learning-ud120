a3b41fa4b08dac5ba0a79999fb645526f91cf41b
aman-am <amanmehta015@gmail.com>
2020-01-20 01:00:00 +0530

    Regression Completed

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -17,19 +17,19 @@ import sys
 import pickle
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
+dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "rb") )
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
-data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
+data = featureFormat( dictionary, features_list, remove_any_zeroes=True, sort_keys = '../tools/python2_lesson06_keys.pkl')
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -39,10 +39,21 @@ test_color = "b"
 ### "r" to differentiate training points from test points.
 
 
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression().fit(feature_train, target_train)
 
+print(reg.score(feature_train,target_train))
+# Gives 0.0455
 
+print(reg.score(feature_test,target_test))
+# Gives -1.48 with bonus vs salary
+# and -0.5927 with bonus vs long_term_incentive in features_list 
 
+print(reg.coef_) 
+# Gives Slope = 5.44814
 
+print(reg.intercept_)
+# Gives intercept = -102360.54
 
 
 ### draw the scatterplot, with color-coded training and testing points
@@ -64,6 +75,9 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b")
+print(reg.coef_) 
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
